5c51d7572ee2656aff2818fb0111a411f909e7f8
Sayuru Dilina <tumiraduli@gmail.com>
2024-10-09 12:08:33 +0530

    first commit

---
+++ src/main/java/edu/icet/controller/LoginFormController.java
@@ -0,0 +1,35 @@
+package edu.icet.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginFormController {
+
+    @FXML
+    private Label lblForgotPw;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+        Stage stage=new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../../../view/dashboard_form_employee.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
